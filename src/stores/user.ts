b08f4780ff7268b1d37b383eb5d68b97cb6fdef8
Neoclassic-alt <boris220-2011@mail.ru>
2024-01-04 20:56:32 +0700

    code refactoring

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -3,6 +3,14 @@ import { defineStore } from 'pinia'
 import type { response as userInfoResponse } from '@/types/api/user'
 import axios from 'axios'
 
+const bonusAllCosts = [
+  { group_name: 'C1', price: 20 },
+  { group_name: 'C2', price: 50 },
+  { group_name: 'C3', price: 100 },
+  { group_name: 'C4', price: 200 },
+  { group_name: 'C5', price: 500 },
+]
+
 export const useUserStore = defineStore('user-info', () => {
   const userInfo = ref<userInfoResponse | null>(null)
 
@@ -15,6 +23,23 @@ export const useUserStore = defineStore('user-info', () => {
     return res.status == 200
   }
 
+  const bonusAvaliableCosts = computed(() => {
+    const _bonusAvaliableCosts = structuredClone(bonusAllCosts)
+
+    getBonusGroups.value?.forEach((item) => 
+      {
+        const index = _bonusAvaliableCosts.findIndex(
+          (bonus_group) => bonus_group.group_name == item.group_name,
+        )
+        if (index + 1) {
+          _bonusAvaliableCosts.splice(index, 1)
+        }
+      }
+    )
+
+    return _bonusAvaliableCosts
+  })
+
   const getUserInfo = computed(() => userInfo.value?.seller)
   const getBonusGroups = computed(() => userInfo.value?.seller.bonus_groups)
 
@@ -22,5 +47,5 @@ export const useUserStore = defineStore('user-info', () => {
     userInfo.value = null
   }
 
-  return { userInfo, getUserInfo, getBonusGroups, fetchUserAndSave, clearUserInfo }
+  return { userInfo, getUserInfo, getBonusGroups, bonusAvaliableCosts, bonusAllCosts, fetchUserAndSave, clearUserInfo }
 })
